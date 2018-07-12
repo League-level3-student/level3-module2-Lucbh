625e2edb3db054313beb1b1ea91718e393426f64
Lucas B <40246581+Lucbh@users.noreply.github.com>
2018-07-12 13:00:38 -0700

    close

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -3,8 +3,69 @@ package _02_More_Algorithms;
 import java.util.List;
 
 public class Algorithms {
+	
+	
 	public static int findBrokenEgg(List<String> eggs) {
+	
+		
+	for (int i = 0; i < eggs.size(); i++) {
+		
+	
+		
+		if (eggs.get(i).equals("cracked")) {
+			
+			System.out.println(i);
+			
+			return i;
+		
+				
+			
+		}
+		
+	
+		
+	
+	
+		}
+	
+	return -1;
+	
 
-		return 0;
+		
+		
+		
+}
+	
+	
+	public static int countPearls(List<Boolean> oysters)	 {
+		
+		int nums = 0;
+		
+		for (int i = 0; i < oysters.size(); i++) {
+			
+			if (oysters.get(i).equals(true)) {
+				nums++;
+			}
+		}
+	return nums;
+}
+	
+	public static int findTallest(List<Double> peeps) {
+		
+		int fire = 0;
+		
+		for (int i = 0; i < peeps.size();i++) {
+			for (int p = 0; p < peeps.size();p++) {
+				
+			if (peeps.get(p) < peeps.get(p + 1)) {
+				
+				return p;
+				
+			}
+			
+			}
+		}
+		return fire;
 	}
+	
 }
